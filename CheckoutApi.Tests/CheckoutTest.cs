d7e804f8621dc1c9da8f931955ca74f64315b8e1
roweigh <roweigh@gmail.com>
2025-07-13 22:16:51 +1200

    added tests

--- CheckoutApi.Tests/CheckoutTest.cs
+++ CheckoutApi.Tests/CheckoutTest.cs
@@ -3,28 +3,89 @@ using Moq;
 using CheckoutApi.Service;
 using CheckoutApi.Repositories;
 using CheckoutApi.Models;
-using CheckoutApi.DTO;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Linq;
-
 namespace CheckoutApi.Tests;
 
 public class CheckoutTest
 {
     private readonly Mock<IProductRepository> _mockRepo;
     private readonly ProductService _service;
-
     public CheckoutTest()
     {
         _mockRepo = new Mock<IProductRepository>();
         _service = new ProductService(_mockRepo.Object);
     }
 
-    [Fact]
-    public async Task EmptyBasket()
+    // Helper function to generate prices indicated by Kata09
+    private List<Product> GetTestProducts()
     {
-        var result = await _service.CheckoutProducts("");
-        Assert.Equal(0, result);
+        return new List<Product>
+        {
+            new Product {
+                Id = 1,
+                ProductName = "A",
+                UnitPrice = 50,
+                DiscountQuantity = 3,
+                DiscountPrice = 130
+            },
+            new Product {
+                Id = 2,
+                ProductName = "B",
+                UnitPrice = 30,
+                DiscountQuantity = 2,
+                DiscountPrice = 45
+            },
+            new Product {
+                Id = 3,
+                ProductName = "C",
+                UnitPrice = 20,
+            },
+            new Product {
+                Id = 4,
+                ProductName = "D",
+                UnitPrice = 15,
+            }
+        };
+    }
+
+    // Helper function to call CheckoutProducts function
+    private async Task<double> Price(string basket)
+    {
+        return await _service.CheckoutProducts(basket);
+    }
+
+    [Fact]
+    public async Task TestTotals()
+    {
+        _mockRepo.Setup(r => r.GetAll()).ReturnsAsync(GetTestProducts());
+
+        Assert.Equal(0, await Price(""));
+        Assert.Equal(50, await Price("A"));
+        Assert.Equal(80, await Price("AB"));
+        Assert.Equal(115, await Price("CDBA"));
+
+        Assert.Equal(100, await Price("AA"));
+        Assert.Equal(130, await Price("AAA"));
+        Assert.Equal(180, await Price("AAAA"));
+        Assert.Equal(230, await Price("AAAAA"));
+        Assert.Equal(260, await Price("AAAAAA"));
+
+        Assert.Equal(160, await Price("AAAB"));
+        Assert.Equal(175, await Price("AAABB"));
+        Assert.Equal(190, await Price("AAABBD"));
+        Assert.Equal(190, await Price("DABABA"));
+    }
+
+    [Fact]
+    public async Task TestIncremental()
+    {
+        _mockRepo.Setup(r => r.GetAll()).ReturnsAsync(GetTestProducts());
+        
+        double checkout = 0;
+        checkout += await Price(""); Assert.Equal(0 , checkout);
+        checkout += await Price("A"); Assert.Equal(50, checkout);
+        checkout += await Price("B"); Assert.Equal(80, checkout);
+        checkout += await Price("A"); Assert.Equal(130, checkout);
+        checkout += await Price("A"); Assert.Equal(160, checkout);
+        checkout += await Price("B"); Assert.Equal(175, checkout);
     }
 }
